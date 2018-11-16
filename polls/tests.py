6974f5e6223f3e9911f4b80519077712ed1c83e4
Rye-Guy <ryanracioppo95@gmail.com>
2018-11-16 18:05:59 -0500

    learning to serve static files with django!

--- polls/tests.py
+++ polls/tests.py
@@ -90,4 +90,30 @@ class QuestionModelTests(TestCase):
                 ['<Question: Past question 2.>', '<Question: Past question 1.>']
             )
 
+    class DetailView(generic.DetailView):
+        def get_queryset(self):
+            """
+            Excludes any questions that arent published yet
+            """
+            return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    class QuestionDetailViewTests(TestCase):
+        def test_future_question(self):
+            """
+            The detail view of a questions with a pub_date in the future returns a 404 error 
+            """
+            future_question = create_question(question_text='Future question.', days=5)
+            url = reverse('polls:detail', args=(future_question.id,))
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 404)
+
+        def test_past_question(self):
+            """
+            The detail view of a question with a pub_date in the past displays the question's text. 
+            """
+            past_question = create_question(question_text='Past Question.', days=-5)
+            url = reverse('polls:detail', args=(past_question.id,))
+            response = self.client.get(url)
+            self.assertContains(response, past_question.question_text)
+
 # Create your tests here.
